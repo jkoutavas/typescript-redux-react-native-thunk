44d55923e00fffb41ac67b65a16b6d3009d88efc
Jay Koutavas <jay.koutavas@isubscribed.com>
2020-03-18 13:47:08 -0400

    Applied correct return type for pageReducer()

--- myTSReduxApp/reducers/pageReducer.ts
+++ myTSReduxApp/reducers/pageReducer.ts
@@ -1,20 +1,33 @@
-import {EReduxActionTypes, IStateModel, AllActions} from '../constants';
+import {
+  EReduxActionTypes,
+  IEmployee,
+  IStateModel,
+  AllActions,
+} from '../constants';
 import {Reducer} from 'redux';
+
+// This module applies some of the material found in
+// https://blog.usejournal.com/writing-better-reducers-with-react-and-typescript-3-4-30697b926ada
 
 const initialState: IStateModel = {
   pageList: [],
 };
 
-const pageReducer: Reducer<IStateModel, AllActions> = (
+function updatePageList(state: IStateModel, payload: Array<IEmployee>) {
+  return <const>{
+    ...state,
+    pageList: payload,
+  };
+}
+type Action = ReturnType<typeof updatePageList> | IStateModel;
+
+const pageReducer: Reducer<Action, AllActions> = (
   state = initialState,
   action,
 ) => {
   switch (action.type) {
     case EReduxActionTypes.GET_PAGE_LIST:
-      return {
-        ...state,
-        pageList: action.payload,
-      };
+      return updatePageList(state, action.payload);
     default:
       return state;
   }
